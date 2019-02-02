d10c9576fa2bf507cb96901bd6c124235fd672c6
javif89 <javier0eduardo@hotmail.com>
2019-02-02 00:40:03 -0500

    Some basic components

--- src/components/ParkingSpot.vue
+++ src/components/ParkingSpot.vue
@@ -1,12 +1,10 @@
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+
 </template>
 
 <script>
     export default {
-        name: 'home',
+        name: 'parking-spot',
         props: {
 
         },
