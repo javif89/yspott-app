bd51e0e153e8d47cab45a72101b7907760a58eeb
javif89 <javier0eduardo@hotmail.com>
2019-02-03 04:59:05 -0500

    Style

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@
 
 <style lang="scss">
   @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css");
-  @import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+  @import url("https://use.fontawesome.com/releases/v5.7.1/css/all.css");
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
--- src/components/BookingConfirmation.vue
+++ src/components/BookingConfirmation.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="modal" tabindex="-1" role="dialog" id="booking-confirmation">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-md" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Confirm Your Booking</h5>
@@ -9,25 +9,23 @@
                     </button>
                 </div>
                 <div class="modal-body container-fluid text-left">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-4">
-                                <h3 class="text-center">Hosted By</h3>
-                                <div class="host-image-confirmation"></div>
-                                <h4 class="text-center mt-2">{{ spot.user.firstName }} {{ spot.user.lastName }}</h4>
-                            </div>
-                            <div class="col">
-                                <img v-if="spot.picture" :src="spot.picture.downloadUrl" alt="" class="img-fluid">
-                                <p v-if="!spot.picture" class="lead text-muted text-center mt-5">No Image Available</p>
-                            </div>
+                    <img v-if="spot.picture" :src="spot.picture.downloadUrl" alt="" class="img-fluid">
+                    <p v-if="!spot.picture" class="lead text-muted text-center mt-5">No Image Available</p>
+                    <hr>
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="host-image-confirmation"></div>
+                        </div>
+                        <div class="col d-flex flex-column justify-content-center">
+                            <h4 class="text-center mt-2">Hosted By: {{ spot.user.firstName }} {{ spot.user.lastName }}</h4>
                         </div>
                     </div>
                     <hr>
-                    <p class="lead text-muted">Please ensure the following information is correct.</p>
+                    <p class="lead text-muted text-center"> Oy! check the information below mate, make sure it is what you want.</p>
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col">
-                                <h5>Address</h5>
+                                <h4>Address</h4>
                                 <div class="parking-spot-address text-left">
                                     <div class="street-line">{{ spot.address.street1 }}<span v-if="spot.address.street2">, {{ spot.address.street2 }}</span></div>
                                     <div class="city-state">{{ spot.address.city }}, {{ spot.address.state }}</div>
@@ -35,7 +33,7 @@
                                     <div class="pricing"> price: ${{spot.price}}</div>
                                 </div>
                             </div>
-                            <div class="col">
+                            <div class="col text-right">
                                 <h4>Order Summary</h4>
                                 <h5>Rate: <small>${{ spot.price }}/hr</small></h5>
                                 <h5>Duration: <small>{{ duration }} Hour(s)</small></h5>
@@ -43,10 +41,12 @@
                             </div>
                         </div>
                         <hr>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" v-model="agreed" id="defaultCheck1">
+                        <div class="form-check text-center" style="font-size: 24px">
+                            <input class="form-check-input" style="display: none;" type="checkbox" v-model="agreed" id="defaultCheck1">
                             <label class="form-check-label" for="defaultCheck1">
-                                I agree to the terms and conditions
+                                <i class="fa fa-check fa-1x" style="color: green" v-if="agreed"></i>
+                                <i class="far fa-square fa-1x" v-if="!agreed"></i>
+                                Okay,Everything looks good!
                             </label>
                         </div>
                     </div>
@@ -80,8 +80,8 @@
 
 <style lang="scss">
     .host-image-confirmation {
-        width: 200px;
-        height: 200px;
+        width: 100px;
+        height: 100px;
         background: url("https://cdn.filestackcontent.com/security=p:eyJjYWxsIjpbInJlYWQiLCJjb252ZXJ0Il0sImV4cGlyeSI6MTU0OTEzMzQ5NDk0OSwiaGFuZGxlIjoiSDVpUnFCUjF5TU9zVkVleHBpakEifQ==,s:4f364b60ecbcfa254ed65229e99b9dac7903cc57b03ac554d739e0d4506f6388/H5iRqBR1yMOsVEexpijA");
         background-position: center;
         border-radius: 50%;
